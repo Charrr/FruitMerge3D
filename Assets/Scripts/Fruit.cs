45054ba737cb0e915d7c5050eebf2a39afd5ba63
Charlie C <chareliecaresnot@gmail.com>
2025-01-01 21:19:19 +0100

    feat: merge fruit to when two of the same kind collide with each other

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace CharlieCares.FruitMerge
@@ -5,12 +6,30 @@ namespace CharlieCares.FruitMerge
     public class Fruit : MonoBehaviour
     {
         private FruitConfig _config;
+        public FruitConfig Config => _config;
+        public event Action<Fruit, Fruit> OnCollidedWithFruit;
 
         public void SetConfig(FruitConfig config)
         {
             transform.localScale = config.Scale * Vector3.one;
-            name = config.Name;
+            name = config.Name + GetInstanceID();
             _config = config;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (_config == null)
+                return;
+
+            if (collision.gameObject.TryGetComponent<Fruit>(out var otherFruit))
+            {
+                // Compare ID to avoid the same collision action being invoked twice.
+                if (otherFruit.Config == _config && otherFruit.GetInstanceID() < GetInstanceID())
+                {
+                    Debug.Log($"{name} collided with {otherFruit.name}.", this);
+                    OnCollidedWithFruit?.Invoke(this, otherFruit);
+                }
+            }
         }
     }
 }
--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -14,15 +14,47 @@ namespace CharlieCares.FruitMerge
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SpawnFruit();
+                SpawnFruitRandomly();
             }
         }
 
-        public void SpawnFruit()
+        public void SpawnFruit(FruitConfig config, Vector3 spawnPos)
         {
             Fruit fruit = Instantiate(_fruitPrefab, _spawnRoot);
-            fruit.transform.position = new Vector3(Random.Range(-4f, 4f), 6f, Random.Range(-4f, 4f));
-            fruit.SetConfig(_fruitConfigs[Random.Range(0, _fruitConfigs.Count)]);
+            fruit.transform.position = spawnPos;
+            fruit.SetConfig(config);
+            fruit.OnCollidedWithFruit += HandleFruitCollision;
+        }
+
+        public void SpawnFruit(string fruitName, Vector3 spawnPos)
+        {
+            FruitConfig chosenFruit = _fruitConfigs.Find(fc => fc.name == fruitName);
+            if (chosenFruit == null)
+            {
+                Debug.LogError($"Fruit type {fruitName} cannot be found.");
+                return;
+            }
+            SpawnFruit(chosenFruit, spawnPos);
+        }
+
+        public void SpawnFruitRandomly()
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-4f, 4f), 6f, Random.Range(-4f, 4f));
+            SpawnFruit(_fruitConfigs[Random.Range(0, _fruitConfigs.Count)], spawnPos);
+        }
+
+        private void HandleFruitCollision(Fruit fruitA, Fruit fruitB)
+        {
+            int configIndex = _fruitConfigs.IndexOf(fruitA.Config);
+            if (configIndex < 0)
+            {
+                Debug.LogError($"Fruit type {fruitA.Config.name} is not registered.", fruitA);
+            }
+            FruitConfig newFruitType = _fruitConfigs[Mathf.Clamp(configIndex + 1, 0, _fruitConfigs.Count - 1)];
+            Vector3 spawnPos = (fruitA.transform.position + fruitB.transform.position) / 2;
+            Destroy(fruitA.gameObject);
+            Destroy(fruitB.gameObject);
+            SpawnFruit(newFruitType, spawnPos);
         }
     }
 }
