86824bb428ff4d3f2a94c8e7f7d0d8be87f4ef3b
Char Chen <charliecaresnot@gmail.com>
2025-01-12 16:37:31 +0100

    refactor: preview fruit position following the cursor on map before dropping

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -9,61 +9,76 @@ namespace CharlieCares.FruitMerge
         [SerializeField] private List<FruitConfig> _fruitConfigs;
         [SerializeField] private TopViewController _topViewMap;
 
+        private Fruit _previewFruit;
+
         private void Update()
         {
+            if (!_previewFruit)
+                return;
+
+            _previewFruit.transform.position = new Vector3(10f * _topViewMap.CursorPosNormalized.x, 6f, 10f * _topViewMap.CursorPosNormalized.y);
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SpawnFruitRandomly();
+                ConfirmDropFruit();
             }
         }
 
         private void Start()
         {
-            _topViewMap.OnClickOnMap += SpawnRandomFruitAtPosition;
+            _topViewMap.OnClickOnMap += HandleClickOnTopViewMap;
+            _previewFruit = SpawnRandomFruit();
         }
 
         private void OnDestroy()
         {
-            _topViewMap.OnClickOnMap -= SpawnRandomFruitAtPosition;
+            _topViewMap.OnClickOnMap -= HandleClickOnTopViewMap;
         }
 
-        public void SpawnFruit(FruitConfig config, Vector3 spawnPos)
+        public Fruit SpawnFruit(FruitConfig config, Vector3 spawnPos = default)
         {
             if (config.Prefab == null)
             {
                 Debug.LogError($"Fruit {config} does not have a prefab, please assgin one to its config.");
-                return;
+                return null;
             }
 
             Fruit fruit = Instantiate(config.Prefab, _spawnRoot);
+            fruit.IsUnderPreview = true;
             fruit.transform.position = spawnPos;
             fruit.SetConfig(config);
             fruit.OnCollidedWithFruit += HandleFruitCollision;
+            return fruit;
         }
 
-        public void SpawnFruit(string fruitName, Vector3 spawnPos)
+        public Fruit SpawnFruit(string fruitName, Vector3 spawnPos = default)
         {
             FruitConfig chosenFruit = _fruitConfigs.Find(fc => fc.name == fruitName);
             if (chosenFruit == null)
             {
                 Debug.LogError($"Fruit type {fruitName} cannot be found.");
-                return;
+                return null;
             }
-            SpawnFruit(chosenFruit, spawnPos);
+            return SpawnFruit(chosenFruit, spawnPos);
         }
 
-        public void SpawnFruitRandomly()
+        public Fruit SpawnRandomFruit()
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-4f, 4f), 6f, Random.Range(-4f, 4f));
-            SpawnFruit(_fruitConfigs[Random.Range(0, _fruitConfigs.Count)], spawnPos);
+            return SpawnFruit(_fruitConfigs[Random.Range(0, _fruitConfigs.Count)]);
         }
 
-        private void SpawnRandomFruitAtPosition(Vector2 posNormalized)
+        private void HandleClickOnTopViewMap(Vector2 posNormalized)
         {
-            float width = 10f;
-            float height = 10f;
-            Vector3 spawnPos = new Vector3(width * posNormalized.x, 6f, height * posNormalized.y);
-            SpawnFruit(_fruitConfigs[Random.Range(0, _fruitConfigs.Count)], spawnPos);
+            ConfirmDropFruit();
+        }
+
+        private void ConfirmDropFruit()
+        {
+            if (_previewFruit == null)
+                return;
+
+            _previewFruit.IsUnderPreview = false;
+            _previewFruit = SpawnRandomFruit();
         }
 
         private void HandleFruitCollision(Fruit fruitA, Fruit fruitB)
